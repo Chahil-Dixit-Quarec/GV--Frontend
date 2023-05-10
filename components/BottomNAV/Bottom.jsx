38bfa0fe9d97bcdf3a46e88942d169cd4c3ef4dc
Chahil Dixit <chahil.dix@gmail.com>
2023-05-10 04:09:42 -0400

    Initial Commit

--- components/BottomNAV/Bottom.jsx
+++ components/BottomNAV/Bottom.jsx
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 // import Link from "next/link";  
 import styles from "../../styles/Bottom.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-  faHome,
-  faPlayCircle,
-  faSearch,
+    faHome,
+    faPlayCircle,
+    faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 function Bottom() {
-  return (
-    <>
-      <div className={styles.IYMAIN}>
-        <Link className={styles.I86iuu} href="/" ><FontAwesomeIcon icon={faHome}></FontAwesomeIcon><br />વાંચો</Link>
-        <Link className={styles.I86iuu} href="/video"><FontAwesomeIcon icon={faPlayCircle}></FontAwesomeIcon><br />જુઓ</Link>
-        <Link className={styles.I86iuu} href="/search"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon><br />શોધો</Link>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.IYMAIN}>
+                <Link className={styles.I86iuu} href="/"><FontAwesomeIcon
+                    icon={faHome}></FontAwesomeIcon><br/>વાંચો</Link>
+                <Link className={styles.I86iuu} href="/video"><FontAwesomeIcon
+                    icon={faPlayCircle}></FontAwesomeIcon><br/>જુઓ</Link>
+                <Link className={styles.I86iuu} href="/search"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon><br/>શોધો</Link>
+            </div>
+        </>
+    );
 }
 
 export default Bottom;
