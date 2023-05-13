064f2b21db95d37c8ce5f7e5bdaaf185b648e00a
Chahil Dixit <chahil.dix@gmail.com>
2023-05-13 17:02:27 -0400

    - View PDF in Browser

--- pages/viewPDF..js
+++ pages/viewPDF..js
@@ -1,19 +1,21 @@
 import {useRouter} from 'next/router';
+import {isMobile} from 'react-device-detect';
 
 function viewPDF(item) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const router = useRouter();
-    const pdfFile = "https://docs.google.com/viewer?url=" + router.query.item + "&embedded=true";
-    console.log(pdfFile);
+    let pdfFile = router.query.item;
+    let renderHTML = '';
+    if (isMobile) {
+        pdfFile = "https://docs.google.com/viewer?url=" + pdfFile + "&embedded=true";
+        renderHTML = <iframe src={pdfFile} frameborder='0' height='500px' width = '100%' > < /iframe>;
+    } else {
+        renderHTML = <object data={pdfFile} type='application/pdf' width='100%' height='100vh'> +
+            <p>If you can't see the pdf, please <a href={pdfFile}>click here</a> to download to PDF </p></object>
+    }
     return (
         <>
-            <object data={pdfFile} type="application/pdf" width="100%"
-                    height="100vh">
-                <p>Alternative text - include a link <a href={pdfFile}>to the
-                    PDF!</a></p>
-            </object>
-            <iframe src={pdfFile} frameborder="0" height="500px"
-                    width="100%"></iframe>
+            {renderHTML}
         </>
     );
 }
