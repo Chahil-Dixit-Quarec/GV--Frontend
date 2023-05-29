40e47b13461a42d35ac63bc751ba4351ba7818ec
Chahil Dixit <chahil.dix@gmail.com>
2023-05-29 19:15:17 -0400

    - Swipe to change video

--- components/Video/video.js
+++ components/Video/video.js
@@ -1,96 +1,94 @@
-import React, { useEffect, useState } from "react";
-import styles from "../../styles/Video.module.css";
-import Image from "next/image";
+import React, {useEffect, useRef, useState} from "react"
+import styles from "../../styles/Video.module.css"
+import newsStyles from "../../styles/Videonews.module.css"
+import Image from 'next/image';
 import axios from "axios";
-// import TimeAgo from "../Timeago/Timeago";
-import { useRouter } from "next/router";
-import Bottom from "../BottomNAV/Bottom";
-import TimeAgo from "react-timeago";
-import En from "react-timeago/lib/language-strings/en";
-import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
-import { isMobile } from "react-device-detect";
+import {useRouter} from 'next/router';
+import Bottom from "../BottomNAV/Bottom"
+import TimeAgo from 'react-timeago';
+import En from 'react-timeago/lib/language-strings/en';
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
+import {isMobile} from 'react-device-detect';
 
 function Video() {
-  const router = useRouter();
-  const [recived, setRecived] = useState([]);
-  const formatter = buildFormatter(En);
-  const handleClick = async (e) => {
-    console.log(e);
-    console.log(encodeURIComponent(JSON.stringify(e)));
-    await router.push({
-      pathname: "/video/[videoNews]",
-      query: { videoNews: encodeURIComponent(JSON.stringify(e)) },
-    });
-  };
-  useEffect(() => {
-    axios
-      .get(process.env.NEXT_PUBLIC_API_BASE_URL + "/getAllVideoData")
-      .then(async (response) => {
-        // console.log(response.data);
-        console.log(isMobile);
-        if (isMobile) {
-          console.log(response.data[0]);
-          return handleClick(response.data[0]);
-        }
-        setRecived(response.data);
-      });
-  }, []);
+    const router = useRouter();
+    const [recived, setRecived] = useState([]);
+    const [videoData, setVideoData] = useState(null);
+    const formatter = buildFormatter(En);
+    const [index, setIndex] = useState(0);
 
-  return (
-    <div className={styles.Tagbody}>
-      <div className={styles.VideoBoundary}>
-        {recived.map((item, index) => (
-          <div
-            className={styles.ImageBoundary}
-            key={item._id}
-            onClick={(e) => {
-              handleClick(item);
-            }}
-          >
-            <div className={styles.ImgBoundaries} key={item._id}>
-              <div key={item._id}>
-                <Image
-                  style={{ borderRadius: "4px" }}
-                  src={process.env.NEXT_PUBLIC_API_URL + `${item.ImagePath}`}
-                  alt="My Image"
-                  width={222}
-                  height={395}
-                />
-              </div>
-              <div className={styles.ef624bc1}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100"
-                  height="100"
-                  viewBox="0 0 24 24"
-                >
-                  <path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" />
-                </svg>
-              </div>
-              <div className={styles.boudriBlack}>
-                <div className={styles.textcontent}>
-                  <div
-                    style={{ color: `${item.Colored}` }}
-                    className={styles.tittle}
-                  >
-                    {item.NewsTittle}
-                  </div>
-                  {/* <div className={styles.subtittle}>{item.NewsSubTittle} </div> */}
-                  <div className={styles.footer}>
-                    <div className={styles.Category}>{item.GujCategory}</div>
-                    <div className={styles.DateTime}>
-                      <TimeAgo date={item.CreatedDate} formatter={formatter} />
+    const handleClick = async (e) => {
+        console.log(e);
+        console.log(encodeURIComponent(JSON.stringify(e)));
+        await router.push({
+            pathname: "/video/[videoNews]",
+            query: {videoNews: encodeURIComponent(JSON.stringify(e))},
+            options: {shallow: true}
+        })
+    }
+
+    useEffect(() => {
+        axios
+            .get(process.env.NEXT_PUBLIC_API_BASE_URL + "/getAllVideoData").then(async (response) => {
+            // console.log(response.data);
+            console.log(isMobile);
+            // if (isMobile) {
+            //     console.log(response.data[index]);
+            //     setVideoData(encodeURIComponent(JSON.stringify(response.data[index])));
+            //     console.log(videoData);
+            // }
+            if (isMobile) {
+                console.log(response.data[0]);
+                return handleClick(response.data[0]);
+            }
+            localStorage.setItem('videos', JSON.stringify(response.data));
+            setRecived(response.data);
+        })
+    })
+
+    return (
+        <div className={styles.Tagbody}>
+            <div className={styles.VideoBoundary}>
+                {recived.map((item, index) => (
+                    <div className={styles.ImageBoundary} key={item._id} onClick={(e) => {
+                        handleClick(item)
+                    }}>
+                        <div className={styles.ImgBoundaries} key={item._id}>
+                            <div key={item._id}>
+                                <Image
+                                    style={{borderRadius: "4px"}}
+                                    src={process.env.NEXT_PUBLIC_API_URL + `${item.ImagePath}`}
+                                    alt="My Image"
+                                    width={222}
+                                    height={395}
+                                />
+                            </div>
+                            <div className={styles.ef624bc1}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"
+                                     viewBox="0 0 24 24">
+                                    <path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z"/>
+                                </svg>
+                            </div>
+                            <div className={styles.boudriBlack}>
+                                <div className={styles.textcontent}>
+                                    <div style={{color: `${item.Colored}`}}
+                                         className={styles.tittle}>{item.NewsTittle}</div>
+                                    <div className={styles.subtittle}>{item.NewsSubTittle} </div>
+                                    <div className={styles.footer}>
+                                        <div className={styles.Category}>{item.GujCategory}</div>
+                                        <div className={styles.DateTime}>
+                                            <TimeAgo date={item.CreatedDate} formatter={formatter}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </div>
-              </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-      <Bottom />
-    </div>
-  );
+            <Bottom/>
+        </div>
+    )
 }
 
-export default Video;
+export default Video
--- components/videoNews/videoNewsed.js
+++ components/videoNews/videoNewsed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react"
 import styles from "../../styles/Videonews.module.css";
 // import TimeAgo from "../Timeago/Timeago";
 import {Icon} from '@iconify/react';
@@ -12,12 +12,103 @@ import TimeAgo from 'react-timeago';
 import En from 'react-timeago/lib/language-strings/en';
 import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
 import {isMobile} from 'react-device-detect';
+import {useSwipeable} from 'react-swipeable';
 
-function videoNewsed(props) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+function VideoNewsed(props) {
     const router = useRouter();
-    const data = JSON.parse(decodeURIComponent(router.query.videoNews));
-    const formatter = buildFormatter(En)
+    const {videoNews} = router.query;
+
+    const formatter = buildFormatter(En);
+    const [previousVideos, setPreviousVideos] = useState([]);
+    const [allVideos, setAllVideos] = useState([]);
+    const [data, setData] = useState(JSON.parse(decodeURIComponent(videoNews)));
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        setData(JSON.parse(decodeURIComponent(videoNews)));
+    }, [videoNews]);
+
+    // let data;
+    // if (isMobile) {
+    //     data = JSON.parse(decodeURIComponent(props.videoNews));
+    // } else {
+    //     data = JSON.parse(decodeURIComponent(router.query.videoNews));
+    // }
+    // data = JSON.parse(decodeURIComponent(router.query.videoNews));
+    // setData(JSON.parse(decodeURIComponent(router.query.videoNews)));
+    const getAllVideos = () => {
+
+        let videos = localStorage.getItem('videos');
+        videos = JSON.parse(videos);
+        setAllVideos(videos);
+        // setAllVideos(JSON.parse('[{"_id":"645de4e8b66bdaf3e6933391","GujCategory":"ટૉપ ન્યૂઝ","EngCategory":"Top news","ImagePath":"Video/Videoimages/2023/5/12/2-01.jpg","Colored":"#FF554B","VideoPath":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4","NewsTittle":"અમદાવાદ ફ્લેટ ધરાશાયી","NewsSubTittle":"ફ્લેટ જર્જરિત હોવાથી મકાન ખાલી કરાયા હતા, કોઈ જાનહાનીના સમાચાર નથી","CreatedDate":"5/12/2023, 12:34:08 PM","__v":0},{"_id":"645de4e8b66bdaf3e6933391","GujCategory":"ટૉપ ન્યૂઝ","EngCategory":"Top news","ImagePath":"Video/Videoimages/2023/5/12/2-01.jpg","Colored":"#FF554B","VideoPath":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4","NewsTittle":" વેજલપુરમાં ત્રણા માળનું ફ્લેટ ધરાશાયી","NewsSubTittle":"ફ્લેટ જર્જરિત હોવાથી મકાન ખાલી કરાયા હતા, કોઈ જાનહાનીના સમાચાર નથી","CreatedDate":"5/12/2023, 12:34:08 PM","__v":0},{"_id":"645de4e8b66bdaf3e6933391","GujCategory":"ટૉપ ન્યૂઝ","EngCategory":"Top news","ImagePath":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4","Colored":"#FF554B","VideoPath":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4","NewsTittle":"અમદાવાદઃ વેજલપુરમાં ત્રણા માળનું","NewsSubTittle":"ફ્લેટ જર્જરિત હોવાથી મકાન ખાલી કરાયા હતા, કોઈ જાનહાનીના સમાચાર નથી","CreatedDate":"5/12/2023, 12:34:08 PM","__v":0},{"_id":"645de4e8b66bdaf3e6933391","GujCategory":"ટૉપ ન્યૂઝ","EngCategory":"Top news","ImagePath":"Video/Videoimages/2023/5/12/2-01.jpg","Colored":"#FF554B","VideoPath":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4","NewsTittle":"અમદાવાદઃ માળનું ફ્લેટ ધરાશાયી","NewsSubTittle":"ફ્લેટ જર્જરિત હોવાથી મકાન ખાલી કરાયા હતા, કોઈ જાનહાનીના સમાચાર નથી","CreatedDate":"5/12/2023, 12:34:08 PM","__v":0},{"_id":"645de4e8b66bdaf3e6933391","GujCategory":"ટૉપ ન્યૂઝ","EngCategory":"Top news","ImagePath":"Video/Videoimages/2023/5/12/2-01.jpg","Colored":"#FF554B","VideoPath":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4","NewsTittle":"ત્રણા માળનું ફ્લેટ ધરાશાયી","NewsSubTittle":"ફ્લેટ જર્જરિત હોવાથી મકાન ખાલી કરાયા હતા, કોઈ જાનહાનીના સમાચાર નથી","CreatedDate":"5/12/2023, 12:34:08 PM","__v":0}]'));
+    }
+    // getAllVideos();
+
+    const handlers = useSwipeable(
+        {
+            // onSwipedDown: () => {
+            //     console.log('swiped')
+            //     if (typeof allVideos[index - 1] !== 'undefined') {
+            //         setPreviousVideos(previousVideos.filter(item => item.VideoPath !== data.VideoPath));
+            //         // setPreviousVideos([...previousVideos, data]);
+            //         data = allVideos[index - 1];
+            //         console.log(data);
+            //     }
+            // },
+            // onSwipedUp: () => {
+            //     console.log('up')
+            //     console.log(allVideos);
+            //     if (typeof allVideos[index + 1] !== 'undefined') {
+            //         setPreviousVideos([...previousVideos, data]);
+            //         data = allVideos[index + 1];
+            //         console.log(data);
+            //     }
+            // },
+            onSwiped: (eventData) => {
+                console.log(eventData);
+                let indexType = eventData.dir === 'Up' ? +1 : -1;
+                console.log('index type', indexType);
+                console.log('index', index);
+                console.log('index + index type', index + indexType);
+                console.log('all videos', allVideos);
+                if (typeof allVideos[index + indexType] !== 'undefined') {
+                    // setPreviousVideos([...previousVideos, data]);
+                    let tempArr = [];
+                    if (eventData.dir === 'Down') {
+                        console.log('up')
+                        setData(previousVideos[previousVideos.length - 1]);
+                        setPreviousVideos(previousVideos.slice(0, -1));
+                        setIndex(index - 1);
+                    } else {
+                        setPreviousVideos([...previousVideos, data]);
+                        setData(allVideos[index + indexType]);
+                        setIndex(index + 1);
+                    }
+
+                    console.log(data);
+                } else {
+                    console.log('not here');
+                }
+            }
+        }
+    )
+
+    // setup ref for your usage
+    const myRef = React.useRef();
+
+    const refPassthrough = (el) => {
+        handlers.ref(el);
+        myRef.current = el;
+    }
+
+    setTimeout(() => {
+        getAllVideos();
+    }, 2000)
+
+    // useEffect(() => {
+    // });
+
     return (
         <>
             <NextSeo
@@ -41,7 +132,7 @@ function videoNewsed(props) {
                     siteName: 'Gujarat Vandan',
                 }}
             />
-            <div className={styles.fullheight}>
+            <div className={styles.fullheight} ref={refPassthrough}>
                 <div className={styles.header}>
                     <div className={`${styles.icon} ${styles.facebook}`}>
                         <Icon
@@ -89,10 +180,11 @@ function videoNewsed(props) {
                 <div className={styles.datarecord}>
                     <h2 className={styles.tittle}>{data.NewsTittle}</h2>
                     <h2 className={styles.sutittle}>{data.NewsSubTittle}</h2>
+                    {/*<h2 className={styles.sutittle}>Test</h2>*/}
                     <div className={styles.subfooter}>
                         <div className={styles.sudata}>{data.GujCategory}</div>
                         <div className={styles.sudata}>
-                            <TimeAgo date={data.CreatedDate} formatter={formatter} />
+                            <TimeAgo date={data.CreatedDate} formatter={formatter}/>
                             {/*<TimeAgo timestamp={data.CreatedDate}/>*/}
                         </div>
                     </div>
@@ -102,5 +194,5 @@ function videoNewsed(props) {
     );
 }
 
-export default videoNewsed
+export default VideoNewsed
 
