501001788675b639de3504598beae4c096a27016
RaviQuarec <ravi.quarec@gmail.com>
2023-05-30 11:26:52 +0530

    responsive changes

--- components/Video/video.js
+++ components/Video/video.js
@@ -37,58 +37,62 @@ function Video() {
   }, []);
 
   return (
-    <div className={styles.Tagbody}>
-      <div className={styles.VideoBoundary}>
-        {recived.map((item, index) => (
-          <div
-            className={styles.ImageBoundary}
-            key={item._id}
-            onClick={(e) => {
-              handleClick(item);
-            }}
-          >
-            <div className={styles.ImgBoundaries} key={item._id}>
-              <div key={item._id}>
-                <Image
-                  style={{ borderRadius: "4px" }}
-                  src={process.env.NEXT_PUBLIC_API_URL + `${item.ImagePath}`}
-                  alt="My Image"
-                  width={222}
-                  height={395}
-                />
-              </div>
-              <div className={styles.ef624bc1}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100"
-                  height="100"
-                  viewBox="0 0 24 24"
-                >
-                  <path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" />
-                </svg>
-              </div>
-              <div className={styles.boudriBlack}>
-                <div className={styles.textcontent}>
-                  <div
-                    style={{ color: `${item.Colored}` }}
-                    className={styles.tittle}
+    <div className={styles.MAbodyt}>
+      <div className={styles.Tagbody}>
+        <div className={styles.VideoBoundary}>
+          {recived.map((item, index) => (
+            <div
+              className={styles.ImageBoundary}
+              key={item._id}
+              onClick={(e) => {
+                handleClick(item);
+              }}
+            >
+              <div className={styles.ImgBoundaries} key={item._id}>
+                <div key={item._id}>
+                  <Image
+                    style={{ borderRadius: "4px" }}
+                    src={process.env.NEXT_PUBLIC_API_URL + `${item.ImagePath}`}
+                    alt="My Image"
+                    width={222}
+                    height={395}
+                  />
+                </div>
+                <div className={styles.ef624bc1}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="100"
+                    height="100"
+                    viewBox="0 0 24 24"
                   >
-                    {item.NewsTittle}
-                  </div>
-                  {/* <div className={styles.subtittle}>{item.NewsSubTittle} </div> */}
-                  <div className={styles.footer}>
-                    <div className={styles.Category}>{item.GujCategory}</div>
-                    <div className={styles.DateTime}>
-                      <TimeAgo date={item.CreatedDate} formatter={formatter} />
+                    <path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" />
+                  </svg>
+                </div>
+                <div className={styles.boudriBlack}>
+                  <div className={styles.textcontent}>
+                    <div
+                      style={{ color: `${item.Colored}` }}
+                      className={styles.tittle}
+                    >
+                      {item.NewsTittle}
+                    </div>
+                    <div className={styles.footer}>
+                      <div className={styles.Category}>{item.GujCategory}</div>
+                      <div className={styles.DateTime}>
+                        <TimeAgo
+                          date={item.CreatedDate}
+                          formatter={formatter}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <Bottom />
       </div>
-      <Bottom />
     </div>
   );
 }
