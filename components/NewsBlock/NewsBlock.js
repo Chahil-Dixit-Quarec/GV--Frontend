ca931dcc1d104d6cdb33199c2d2644697c667a56
Chahil Dixit <chahil.dix@gmail.com>
2023-06-23 01:52:55 -0400

    - env changes

--- components/NewsBlock/NewsBlock.js
+++ components/NewsBlock/NewsBlock.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styles from "../../styles/NewsBlock.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLink} from "@fortawesome/free-solid-svg-icons";
@@ -20,6 +20,7 @@ function NewsBlock(props) {
     const [total, setTotal] = useState(0);
     const [threshold, setThreshold] = useState(0);
     const pageSize = 10;
+    const [pagination, setPagination] = useState(0);
     const facebookClick = (url) => {
         const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
         window.open(shareUrl, "_blank");
@@ -61,149 +62,163 @@ function NewsBlock(props) {
     };
 
     const getData = async () => {
-        let startPage = pageSize * start;
-        console.log(startPage);
-        axios.post(process.env.NEXT_PUBLIC_API_BASE_URL + "/allNewsNew", {start: startPage, pageSize})
-            .then(async (response) => {
-                if (response.data.data.length > 0) {
-                    let tempArr = [];
-                    tempArr = [...newsDatas, ...response.data.data];
-                    tempArr = _.uniq(tempArr, x => x.NewsTittle);
-                    await setNewsDatas(tempArr);
-                    setDataStatus('success');
-                    setTotal(response.data.total);
-                    setStart(start + 1);
-                    setThreshold(Math.ceil(response.data.total / 10));
-                    console.log(Math.ceil(response.data.total / 10));
-                }
-            })
-    }
-
-    useEffect(() => {
-        setNewsDatas([]);
-        props.value.unique === true
-            ? getData() : axios.post(process.env.NEXT_PUBLIC_API_BASE_URL + "/allNewsData", {
+        if (props.value.unique === true) {
+            let startPage = pageSize * start;
+            console.log(startPage, 'start page');
+            axios.post(process.env.NEXT_PUBLIC_API_BASE_URL + "/allNewsNew", {start: startPage, pageSize})
+                .then(async (response) => {
+                    if (response.data.data.length > 0) {
+                        let tempArr = [];
+                        tempArr = [...newsDatas, ...response.data.data];
+                        tempArr = _.uniq(tempArr, x => x.NewsTittle);
+                        console.log(tempArr.length, 'temp arr')
+                        await setNewsDatas(tempArr);
+                        setDataStatus('success');
+                        setTotal(response.data.total);
+                        setStart(start + 1);
+                        console.log(start, 'start');
+                        setThreshold(Math.ceil(response.data.total / 10));
+                        console.log(Math.ceil(response.data.total / 10));
+                    } else {
+                        console.log('else');
+                    }
+                })
+        } else {
+            axios.post(process.env.NEXT_PUBLIC_API_BASE_URL + "/allNewsData", {
                 data: `${props.value.data}`,
             }).then(async (response) => {
                 await setNewsDatas(response.data.response);
                 console.log("res2");
             });
-    }, [props.value.unique, props.value.data]);
+        }
+    }
+
+    const observerTarget = useRef(null);
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                if (entries[0].isIntersecting) {
+                    getData();
+                }
+            },
+            {threshold: 1}
+        );
+
+        if (observerTarget.current) {
+            observer.observe(observerTarget.current);
+        }
+
+        return () => {
+            if (observerTarget.current) {
+                observer.unobserve(observerTarget.current);
+            }
+        };
+    }, [props.value.unique, props.value.data, observerTarget]);
 
     const loadMore = <div key={0}>
-        <button className={styles.loadMoreBtn} onClick={getData}>Load More</button>
+        <span style={{textAlign: "center"}}>Loading...</span>
     </div>
 
     return (
-        <div style={{textAlign: "center", height: 300, overflow: "auto"}}>
+        <>
             <ToastContainer/>
             {dataStatus === "success" && (
-                <InfiniteScroll
-                    dataLength={total}
-                    hasMore={true}
-                    loader={loadMore}
-                    useWindow={true}
-                    scrollThreshold={threshold}
-                    height={300}
-                    endMessage={<p>No more data to load.</p>}
-                    next={getData}>
-                    {newsDatas
-                        // .slice(0)
-                        .reverse()
-                        .map((news, index) => (
-                            <div className={styles.BlockHead} key={index} style={{textAlign: "left"}}>
-                                <div
-                                    className={styles.refl}
-                                    onClick={(e) => {
-                                        handleClick({
-                                            _id: news._id,
-                                            data: news.NewsSubTittle,
-                                            image: news.Path,
-                                            category: news.EngCategory,
-                                        });
-                                    }}
-                                >
-                                    <div className={styles.headlinesright}>
-                                        <Image
-                                            // src={img}
-                                            src={process.env.NEXT_PUBLIC_API_URL + `${news.Path}`}
-                                            alt={"data"}
-                                            width={500}
-                                            height={500}
-                                        />
-                                    </div>
-                                    <div className={styles.headlinesleft}>
-                                        <h3>
-                                            <font
-                                                style={{color: news.Colored ? news.Colored : "#000000"}}
-                                            >
-                                                {news.NewsTittle.slice(0, 52)}
-                                            </font>
-                                            {news.NewsSubTittle}
-                                        </h3>
-                                    </div>
+                <>
+                    {newsDatas.map((news, index) => (
+                        <div className={styles.BlockHead} key={index} style={{textAlign: "left"}}>
+                            <div
+                                className={styles.refl}
+                                onClick={(e) => {
+                                    handleClick({
+                                        _id: news._id,
+                                        data: news.NewsSubTittle,
+                                        image: news.Path,
+                                        category: news.EngCategory,
+                                    });
+                                }}
+                            >
+                                <div className={styles.headlinesright}>
+                                    <Image
+                                        // src={img}
+                                        src={process.env.NEXT_PUBLIC_API_URL + `${news.Path}`}
+                                        alt={"data"}
+                                        width={500}
+                                        height={500}
+                                    />
+                                </div>
+                                <div className={styles.headlinesleft}>
+                                    <h3>
+                                        <font
+                                            style={{color: news.Colored ? news.Colored : "#000000"}}
+                                        >
+                                            {news.NewsTittle.slice(0, 52)}
+                                        </font>
+                                        {news.NewsSubTittle}
+                                    </h3>
+                                </div>
+                            </div>
+
+                            <div className={styles.NewFooter2}>
+                                <div className="cated">
+                                    {news.GujSubCategory ? news.GujSubCategory : news.GujCategory}
                                 </div>
 
-                                <div className={styles.NewFooter2}>
-                                    <div className="cated">
-                                        {news.GujSubCategory ? news.GujSubCategory : news.GujCategory}
+                                <div className={styles.SocialIcon2}>
+                                    <div
+                                        onClick={(e) => {
+                                            toastOnClick(process.env.NEXT_PUBLIC_FRONT_FILES +
+                                                "category/" + news.EngCategory + "/" + news._id
+                                            );
+                                        }}
+                                    >
+                                        <FontAwesomeIcon
+                                            className={styles.SocialIconed1}
+                                            href="#"
+                                            icon={faLink}
+                                        ></FontAwesomeIcon>
                                     </div>
-
-                                    <div className={styles.SocialIcon2}>
-                                        <div
-                                            onClick={(e) => {
-                                                toastOnClick(process.env.NEXT_PUBLIC_FRONT_FILES +
-                                                    "category/" + news.EngCategory + "/" + news._id
-                                                );
-                                            }}
-                                        >
-                                            <FontAwesomeIcon
-                                                className={styles.SocialIconed1}
-                                                href="#"
-                                                icon={faLink}
-                                            ></FontAwesomeIcon>
-                                        </div>
-                                        <div
-                                            onClick={(e) => {
-                                                facebookClick(
-                                                    process.env.NEXT_PUBLIC_FRONT_FILES +
-                                                    "category/" +
-                                                    news.EngCategory +
-                                                    "/" +
-                                                    news._id
-                                                );
-                                            }}
-                                        >
-                                            <FontAwesomeIcon
-                                                className={styles.SocialIconed2}
-                                                href="#"
-                                                icon={faFacebook}
-                                            ></FontAwesomeIcon>
-                                        </div>
-                                        <div
-                                            onClick={(e) => {
-                                                twitterClick(
-                                                    process.env.NEXT_PUBLIC_FRONT_FILES +
-                                                    "category/" +
-                                                    news.EngCategory +
-                                                    "/" +
-                                                    news._id
-                                                );
-                                            }}
-                                        >
-                                            <FontAwesomeIcon
-                                                className={styles.SocialIconed2}
-                                                href="#"
-                                                icon={faTwitter}
-                                            ></FontAwesomeIcon>
-                                        </div>
+                                    <div
+                                        onClick={(e) => {
+                                            facebookClick(
+                                                process.env.NEXT_PUBLIC_FRONT_FILES +
+                                                "category/" +
+                                                news.EngCategory +
+                                                "/" +
+                                                news._id
+                                            );
+                                        }}
+                                    >
+                                        <FontAwesomeIcon
+                                            className={styles.SocialIconed2}
+                                            href="#"
+                                            icon={faFacebook}
+                                        ></FontAwesomeIcon>
+                                    </div>
+                                    <div
+                                        onClick={(e) => {
+                                            twitterClick(
+                                                process.env.NEXT_PUBLIC_FRONT_FILES +
+                                                "category/" +
+                                                news.EngCategory +
+                                                "/" +
+                                                news._id
+                                            );
+                                        }}
+                                    >
+                                        <FontAwesomeIcon
+                                            className={styles.SocialIconed2}
+                                            href="#"
+                                            icon={faTwitter}
+                                        ></FontAwesomeIcon>
                                     </div>
                                 </div>
                             </div>
-                        ))}
-                </InfiniteScroll>
+                        </div>
+                    ))}
+                </>
             )}
-        </div>
+            <div ref={observerTarget}></div>
+        </>
     );
 }
 
