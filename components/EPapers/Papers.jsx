c04ccc19f6186a569c6b8c1a5be6f9cab81cad81
Chahil Dixit <chahil.dix@gmail.com>
2023-07-03 03:04:33 -0400

    - EPaper Pagination - Video Dynamic Fix

--- components/EPapers/Papers.jsx
+++ components/EPapers/Papers.jsx
@@ -1,65 +1,110 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styles from "../../styles/Papers.module.css";
-import PapersIMG from "./IMG/Papers.png";
 import axios from "axios";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
+import _, { map } from "underscore";
 
 function Papers() {
-  const router = useRouter();
-  const [newsData, setNewsData] = useState([]);
+    const router = useRouter();
+    const [newsData, setNewsData] = useState([]);
+    const [dataStatus, setDataStatus] = useState("");
+    const [start, setStart] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [threshold, setThreshold] = useState(0);
+    const pageSize = 18;
+    const [pagination, setPagination] = useState(0);
 
-  let record = newsData.sort(
-    (a, b) =>
-      new Date(...a.NewsPaperDate.split("-").reverse()) -
-      new Date(...b.NewsPaperDate.split("-").reverse())
-  );
+    const download = (e) => {
+        router.push({
+            pathname: "/viewPDF",
+            query: {item: process.env.NEXT_PUBLIC_API_URL + e},
+        });
+    };
 
-  const download = (e) => {
-    router.push({
-      pathname: "/viewPDF",
-      query: { item: process.env.NEXT_PUBLIC_API_URL + e },
-    });
-  };
+    const getData = async () => {
+        let startPage = pageSize * start;
+        console.log(startPage, "start page");
+        axios.get(process.env.NEXT_PUBLIC_API_BASE_URL + "/newsPaperNew", {
+            params: {
+                start: startPage,
+                pageSize,
+            }
+        }).then(async (response) => {
+            console.log(response)
+            if (response.data.data.length > 0) {
+                let tempArr = [];
+                tempArr = [...newsData, ...response.data.data];
+                tempArr = _.uniq(tempArr, (x) => x.NewsPaperDate);
+                await setNewsData(tempArr);
+                setDataStatus("success");
+                setTotal(response.data.total);
+                setStart(start + 1);
+                console.log(start, "start");
+                setThreshold(Math.ceil(response.data.total / 10));
+                console.log(Math.ceil(response.data.total / 10));
+            } else {
+                console.log("else");
+            }
+        });
+    }
 
-  useEffect(() => {
-    axios
-      .get(process.env.NEXT_PUBLIC_API_BASE_URL + "/newsPaper")
-      .then(async (response) => {
-        await setNewsData(response.data.response);
-      });
-  }, []);
+    const [firstUse, setFirstUse] = useState(false);
 
-  return (
-    <>
-      <div className={styles.EPapers}>
-        <h2>ઈ-ન્યુઝ પેપર્સ</h2>
-      </div>
-      <div className={styles.Collection}>
-        {record
-          .slice(0)
-          .reverse()
-          .map((news, index) => (
-            <div className={styles.upload} key={index}>
-              <button onClick={(e) => download(news.Path)}>
-                <Image
-                  src={process.env.NEXT_PUBLIC_API_URL + `${news.PosterPath}`}
-                  alt=""
-                  height="121"
-                  width="195"
-                  // style={{ cursor: "pointer" }}
-                />
-                <p>
-                  તારીખ:-{news.NewsPaperDate}
-                  <br />
-                  દિવસ:-{news.Day ? news.Day : "રવિવાર"}
-                </p>
-              </button>
+    const observerTarget = useRef(null);
+    useEffect(() => {
+        if (firstUse === false) {
+            getData().then(() => setFirstUse(true));
+        }
+        const observer = new IntersectionObserver(
+            async (entries) => {
+                if (entries[0].isIntersecting && total < newsData.length) {
+                    console.log("2");
+                    await getData();
+                }
+            },
+            {threshold: 1}
+        );
+
+        if (observerTarget.current) {
+            observer.observe(observerTarget.current);
+        }
+
+        return () => {
+            if (observerTarget.current) {
+                observer.unobserve(observerTarget.current);
+            }
+        };
+    }, [observerTarget, start]);
+
+    return (
+        <>
+            <div className={styles.EPapers}>
+                <h2>ઈ-ન્યુઝ પેપર્સ</h2>
             </div>
-          ))}
-      </div>
-    </>
-  );
+            <div className={styles.Collection}>
+                {newsData
+                    .map((news, index) => (
+                        <div className={styles.upload} key={index}>
+                            <button onClick={(e) => download(news.Path)}>
+                                <Image
+                                    src={process.env.NEXT_PUBLIC_API_URL + `${news.PosterPath}`}
+                                    alt=""
+                                    height="121"
+                                    width="195"
+                                />
+                                <p>
+                                    તારીખ:-{news.NewsPaperDate}
+                                    <br/>
+                                    દિવસ:-{news.Day ? news.Day : "રવિવાર"}
+                                </p>
+                            </button>
+                        </div>
+                    ))}
+            </div>
+            {total > 0 && <div ref={observerTarget}></div>}
+        </>
+    );
 }
 
 export default Papers;
--- components/videoNews/videoNewsed.js
+++ components/videoNews/videoNewsed.js
@@ -11,40 +11,72 @@ import {
     Facebook,
     HighlightOffOutlined,
     LinkSharp,
-    ShareOutlined, Speaker,
-    Twitter, VolumeMute, VolumeMuteRounded, VolumeOffRounded, VolumeUpOutlined, VolumeUpRounded, WhatsApp
+    ShareOutlined,
+    Speaker,
+    Twitter,
+    VolumeMute,
+    VolumeMuteRounded,
+    VolumeOffRounded,
+    VolumeUpOutlined,
+    VolumeUpRounded,
+    WhatsApp
 } from "@material-ui/icons";
+import axios from "axios";
 
 function VideoNewsed(props) {
     const router = useRouter();
-    const {videoNews} = router.query;
+    const {push, query} = useRouter();
+    let {videoNews} = router.query;
 
     const formatter = buildFormatter(En);
     const [previousVideos, setPreviousVideos] = useState([]);
     const [allVideos, setAllVideos] = useState([]);
-    const [data, setData] = useState(JSON.parse(decodeURIComponent(videoNews)));
+    const [data, setData] = useState({});
     const [index, setIndex] = useState(0);
     const [activateListener, setActivateListener] = useState(false);
 
     const [show, setShow] = useState(false);
     const [showClass, setShowClass] = useState('close');
+    const [videoName, setVideoName] = useState('');
 
     useEffect(() => {
-        setData(JSON.parse(decodeURIComponent(videoNews)));
-
+        console.log('called')
+        // axios.get(process.env.NEXT_PUBLIC_API_BASE_URL + "/getVideo", {
+        //     params: {
+        //         name: decodeURIComponent(videoNews)
+        //     }
+        // }).then(async (response) => {
+        //     setData(response.data.data)
+        //     setVideoName(router.query.videoNews)
+        // });
+        getVideoData(decodeURIComponent(videoNews))
         setTimeout(() => {
             getAllVideos();
         }, 2000)
-    }, [videoNews]);
+    }, [videoName]);
 
     const getAllVideos = () => {
-        let videos = localStorage.getItem('videos');
-        videos = JSON.parse(videos);
-        setAllVideos(videos);
+        axios.get(process.env.NEXT_PUBLIC_API_BASE_URL + "/getAllVideoDataId").then(async (response) => {
+            let data = response.data;
+            // await data.shift()
+            await setAllVideos(data)
+        });
+    }
+
+    const getVideoData = name => {
+        axios.get(process.env.NEXT_PUBLIC_API_BASE_URL + "/getVideo", {
+            params: {
+                name
+            }
+        }).then(async (response) => {
+            setData(response.data.data)
+            // setVideoName(router.query.videoNews)
+        });
     }
 
     const whatsappShare = () => {
-        window.open('https://wa.me/?text=' + process.env.NEXT_PUBLIC_FRONT_FILES + 'video/' + encodeURIComponent(videoNews).trim(), '_blank');
+        window.open('https://wa.me/?text=' + process.env.NEXT_PUBLIC_FRONT_FILES + 'video/'
+            + encodeURIComponent(videoNews).trim(), '_blank');
     }
 
     const refVideo = useRef(null);
@@ -64,8 +96,7 @@ function VideoNewsed(props) {
         videoRef.current = el;
     }
     const handlers = useSwipeable({
-        onSwiped: (eventData) => {
-            console.log(eventData);
+        onSwiped: async (eventData) => {
             let indexType = eventData.dir === 'Up' ? +1 : -1;
             console.log('index type', indexType);
             console.log('index', index);
@@ -74,16 +105,24 @@ function VideoNewsed(props) {
             if (typeof allVideos[index + indexType] !== 'undefined') {
                 let tempArr = [];
                 if (eventData.dir === 'Down') {
-                    console.log('up')
+                    console.log('down')
                     setData(previousVideos[previousVideos.length - 1]);
                     setPreviousVideos(previousVideos.slice(0, -1));
                     setIndex(index - 1);
                 } else {
+                    console.log('up');
+                    // setVideoName(encodeURIComponent(allVideos[index + indexType].NewsTittle))
+                    getVideoData(allVideos[index + indexType].NewsTittle)
                     setPreviousVideos([...previousVideos, data]);
-                    setData(allVideos[index + indexType]);
+                    // setData(allVideos[index + indexType]);
                     setIndex(index + 1);
                 }
-                console.log(data);
+                await push({
+                    query: {
+                        videoNews: allVideos[index + indexType].NewsTittle
+                    }
+                }, undefined, {shallow: true});
+                // console.log(data);
             } else {
                 console.log('not here');
             }
@@ -105,75 +144,71 @@ function VideoNewsed(props) {
         }
     }
 
-    return (<>
-        <NextSeo
-            title={data.NewsTittle}
-            description={data.NewsSubTittle}
-            canonical={process.env.NEXT_PUBLIC_API_URL + data.NewsTittle}
-            openGraph={{
-                url: process.env.NEXT_PUBLIC_API_URL + data.VideoPath,
-                title: data.NewsTittle,
-                description: data.NewsSubTittle,
-                images: [{
-                    url: process.env.NEXT_PUBLIC_API_URL + data.ImagePath,
-                    width: 800,
-                    height: 600,
-                    alt: 'Og Image Alt',
-                    type: 'image/jpeg',
-                }, {url: process.env.NEXT_PUBLIC_API_URL + data.ImagePath},],
-                siteName: 'Gujarat Vandan',
-            }}
-        />
-        <div className={styles.app__videos} ref={videoRefFn}>
-            <div className={styles.video}>
-                <video className={styles.video__player} ref={refVideo}
-                       src={process.env.NEXT_PUBLIC_API_URL + data.VideoPath} loop
-                       autoPlay controls disablePictureInPicture playsInline controlsList="nodownload"></video>
-                {isMobile &&
-                    <div className={styles.videoSidebar}>
-                        <div className={styles.videoSidebar__button} onClick={whatsappShare}>
-                            <WhatsApp></WhatsApp>
-                        </div>
-                        <div className={styles.videoSidebar__button} onClick={videoMuteUnMute}>
-                            {!videoMute &&
-                                <VolumeUpRounded></VolumeUpRounded>
-                            }
-                            {videoMute &&
-                                <VolumeOffRounded></VolumeOffRounded>
-                            }
+    return (
+        <>
+            {data &&
+                <>
+                    <NextSeo
+                        title={data.NewsTittle}
+                        description={data.NewsSubTittle}
+                        canonical={process.env.NEXT_PUBLIC_API_URL + data.NewsTittle}
+                        openGraph={{
+                            url: process.env.NEXT_PUBLIC_API_URL + data.VideoPath,
+                            title: data.NewsTittle,
+                            description: data.NewsSubTittle,
+                            images: [{
+                                url: process.env.NEXT_PUBLIC_API_URL + data.ImagePath,
+                                width: 800,
+                                height: 600,
+                                alt: 'Og Image Alt',
+                                type: 'image/jpeg',
+                            }, {url: process.env.NEXT_PUBLIC_API_URL + data.ImagePath},],
+                            siteName: 'Gujarat Vandan',
+                        }}
+                    />
+                    <div className={styles.app__videos} ref={videoRefFn}>
+                        <div className={styles.video}>
+                            <video className={styles.video__player} ref={refVideo}
+                                   src={process.env.NEXT_PUBLIC_API_URL + data.VideoPath} loop
+                                   autoPlay controls disablePictureInPicture playsInline
+                                   controlsList="nodownload"></video>
+                            {isMobile && <div className={styles.videoSidebar}>
+                                <div className={styles.videoSidebar__button} onClick={whatsappShare}>
+                                    <WhatsApp></WhatsApp>
+                                </div>
+                                <div className={styles.videoSidebar__button} onClick={videoMuteUnMute}>
+                                    {!videoMute && <VolumeUpRounded></VolumeUpRounded>}
+                                    {videoMute && <VolumeOffRounded></VolumeOffRounded>}
+                                </div>
+                            </div>}
+                            {!isMobile && <div className={styles.videoSidebar}>
+                                <div className={styles.videoSidebar__button}>
+                                    <Facebook></Facebook>
+                                </div>
+                                <div className={styles.videoSidebar__button}>
+                                    <Twitter></Twitter>
+                                </div>
+                                <div className={styles.videoSidebar__button}>
+                                    <LinkSharp></LinkSharp>
+                                </div>
+                            </div>}
+                            {!isMobile && <div className={styles.videoSidebar__close__button}
+                                               onClick={async (e) => router.back()}>
+                                <HighlightOffOutlined></HighlightOffOutlined>
+                            </div>}
+                            <div className={`${styles.videoFooter} ${showClass ? styles.close : styles.open}`}
+                                 onClick={showHide} ref={divRef}>
+                                <div className={`${styles.videoFooter__text}`}>
+                                    <h3>{data.NewsTittle}</h3>
+                                    <p className={styles.videoFooter__description}>{data.NewsSubTittle}</p>
+                                    <p><TimeAgo date={data.CreatedDate} formatter={formatter}/></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                }
-                {!isMobile &&
-                    <div className={styles.videoSidebar}>
-                        <div className={styles.videoSidebar__button}>
-                            <Facebook></Facebook>
-                        </div>
-                        <div className={styles.videoSidebar__button}>
-                            <Twitter></Twitter>
-                        </div>
-                        <div className={styles.videoSidebar__button}>
-                            <LinkSharp></LinkSharp>
-                        </div>
-                    </div>
-                }
-                {!isMobile &&
-                    <div className={styles.videoSidebar__close__button}
-                         onClick={async (e) => router.back()}>
-                        <HighlightOffOutlined></HighlightOffOutlined>
-                    </div>
-                }
-                <div className={`${styles.videoFooter} ${showClass ? styles.close : styles.open}`}
-                     onClick={showHide} ref={divRef}>
-                    <div className={`${styles.videoFooter__text}`}>
-                        <h3>{data.NewsTittle}</h3>
-                        <p className={styles.videoFooter__description}>{data.NewsSubTittle}</p>
-                        <p><TimeAgo date={data.CreatedDate} formatter={formatter}/></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </>);
+                </>
+            }
+        </>);
 }
 
 export default VideoNewsed
